25f8d657412ba9530702de682e24a1067e7c9223
Nikaberry <veronikasmolkova@yandex.ru>
2024-01-08 15:23:07 +0300

    1

--- start_end.py
+++ start_end.py
@@ -1,1 +1,40 @@
 import pygame
+import os
+import sys
+
+
+clock = pygame.time.Clock()
+height = 600
+width = 1100
+screen = pygame.display.set_mode((width, height))
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+def before():
+    intro_text = ['Перемещение героя',
+                  'Герой двигается',
+                  'Карта на месте']
+    fon = (230, 230, 230), (500, 500)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(50)
